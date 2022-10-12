5ebb9dff4c85b47019e66dd74bd19c291fdd7a8d
lijiatai <lijiataihpu@gmail.com>
2022-10-12 09:22:59 +0800

    slider组件搭建

--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -1,25 +1,61 @@
 <template>
-	<div>recommend</div>
+	<div class="recommend">
+		<div class="slider-wrapper">
+			<div class="slider-content">
+				<Slider v-if="sliders.length" :sliders="sliders"></Slider>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 import { getRecommend } from '@/service/recommend'
+import Slider from '@/components/base/slider/slider'
 
 export default {
 	name: 'recommend',
 
-	data () {
-		return {}
+	components: {
+		Slider
+	},
+
+	data() {
+		return {
+			sliders: [],
+			albums: []
+		}
 	},
 
 	async created() {
 		const result = await getRecommend()
 
-		console.log(18, result)
+		this.sliders = result.sliders
+		this.albums = result.albums
 	}
 }
 </script>
 
 <style lang="scss" scoped>
+.recommend {
+	position: fixed;
+	width: 100%;
+	top: 88px;
+	bottom: 0;
 
+	.slider-wrapper {
+		position: relative;
+        width: 100%;
+        height: 0;
+        padding-top: 40%;
+        overflow: hidden;
+
+		.slider-content {
+			position: absolute;
+			left: 0;
+			top: 0;
+			width: 100%;
+			height: 100%;
+        }
+	}
+}
 </style>
