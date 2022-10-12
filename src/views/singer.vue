df91caca8f774a1b5761ac6679871c10128b9982
lijiatai <lijiataihpu@gmail.com>
2022-10-12 17:38:09 +0800

    获取歌手列表

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -3,11 +3,19 @@
 </template>
 
 <script>
+import { getSingerList } from '@/service/singer'
+
 export default {
 	name: 'singer',
 
 	data () {
 		return {}
+	},
+
+	async created() {
+		const result = await getSingerList()
+
+		console.log(18, result)
 	}
 }
 </script>
