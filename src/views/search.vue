be4d449f09ffa2827edb2ee6eeb0e38680af2a79
lijiatai <lijiataihpu@gmail.com>
2022-10-15 14:22:27 +0800

    search-inputsearch-input组件开发

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,17 +1,47 @@
 <template>
-	<div>search</div>
+	<div class="search">
+		<div class="search-input-wrapper">
+			<searchInput v-model="query"></searchInput>
+		</div>
+	</div>
 </template>
 
 <script>
+import searchInput from '@/components/search/search-input'
+import { ref, watch } from 'vue'
+
 export default {
 	name: 'search',
 
-	data () {
-		return {}
+	components: {
+		searchInput
+	},
+
+	setup() {
+		const query = ref('')
+
+		watch(query, (val) => {
+			console.log(val)
+		})
+
+		return {
+			query
+		}
 	}
 }
 </script>
 
 <style lang="scss" scoped>
+.search {
+    position: fixed;
+    width: 100%;
+    top: 88px;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
 
+	.search-input-wrapper {
+		margin: 20px;
+    }
+  }
 </style>
